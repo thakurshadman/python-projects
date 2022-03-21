7b86edd4eb91b1a25faa80c4a383e43c6d30ada6
shadman <thakurshadman@outlook.com>
2022-03-21 00:40:48 -0400

    Implemented win condition

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -4,31 +4,96 @@ import random
 print('WELCOME TO HANGMAN!\n')
 
 categories = ["Animal", "Sport", "Food"]
-categories_entries = {"Animal": ["cat", "dog", "elephant", "chicken", "mouse", "tiger", "horse", "fish", "python"], "Sport": [
+
+word_bank = {"Animal": ["cat", "dog", "elephant", "chicken", "mouse", "tiger", "horse", "fish", "python"], "Sport": [
     'soccer', 'basketball', 'golf', 'hockey', 'cricket', 'football', 'tennis', 'boxing'], "Food": ['pizza', 'burger', 'cake', 'pasta', 'samosa', 'chocolate', 'steak', 'kebab', 'taco']}
+
+hangman_stage = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
 
 for order, category in enumerate(categories):
     print(order, category)
+    
 print('\n')
-users_cat_choice = int(
-    input("Please enter the related number to select a category!: "))
-entries = categories_entries[categories[users_cat_choice]]
-word_to_guess = entries[random.randint(0, len(entries)-1)]
-print(f"\nI'm a {len(word_to_guess)} letter {categories[users_cat_choice]}!")
-blank_placeholders = ''
-for i in word_to_guess:
-    blank_placeholders += '_ '
 
-print(blank_placeholders + '\n')
+users_cat_choice = int(input("Please enter the related number to select a category!: "))
+entries_from_categories = word_bank[categories[users_cat_choice]]
+word_to_guess = list(entries_from_categories[random.randint(0, len(entries_from_categories)-1)])
+
+print(word_to_guess)
+print(f"\nI'm a {len(word_to_guess)} letter {categories[users_cat_choice]}!")
+
+blank_placeholders = []
+for i in word_to_guess:
+    blank_placeholders.append('_')
+
+print(' '.join(blank_placeholders) + '\n')
 
 incorrect_guesses = set()
-correct_guesses = set()
-users_letter = input('Guess a letter: ')
+while len(incorrect_guesses) < 6:
+    print(hangman_stage[len(incorrect_guesses)])
+    print(f'Incorrect guesses: {"{}" if not incorrect_guesses else incorrect_guesses}\n')
+    users_letter = input('Guess a letter: ')
+    for i in range(len(word_to_guess)):
+        if word_to_guess[i] == users_letter:
+            blank_placeholders[i] = users_letter
+    if users_letter not in blank_placeholders:
+        incorrect_guesses.add(users_letter)
+    updated_placeholder_word = ' '.join(blank_placeholders)
+    print('\n'+ updated_placeholder_word)
+    if blank_placeholders == word_to_guess:
+        print("Hurray you won!")
+        exit()
+    
+print(hangman_stage[len(incorrect_guesses)])
+print('You lose!')
 
-for i in range(len(word_to_guess)):
-    if word_to_guess[i] == users_letter:
-        correct_guesses.add(users_letter)
-        splitted = list(blank_placeholders)
-        splitted[i*2] = users_letter
-        blank_placeholders = ''.join(splitted)
-print(blank_placeholders)
+
