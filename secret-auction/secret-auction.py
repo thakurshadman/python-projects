adf4de32622b9df2d709d33b92f87010c33ee9dc
shadman <thakurshadman@outlook.com>
2022-03-29 00:39:52 -0400

    try catch added for prompt_others

--- secret-auction/secret-auction.py
+++ secret-auction/secret-auction.py
@@ -1,6 +1,7 @@
-
+from os import system, name
 
 def Greeting():
+    
     gavel = '''
                          ___________
                          \         /
@@ -19,14 +20,25 @@ Welcome to the secret auction!
     print(gavel)
     
 def prompt_name()->str:
+    
     name = input("What is your name?: ")
     return name
 
 def prompt_bid()->int:
-    bid = int(input('What is your bid?: '))
-    return bid
+    
+    input_is_valid = False
+    while not input_is_valid:
+        try:
+            bid = int(input('What is your bid?: $'))
+            if bid > -1:
+                return bid
+            else:
+                raise ValueError()
+        except ValueError:
+            print("Invalid Input!\n")
     
 def prompt_other_participants()->bool:
+    
     input_is_valid = False
     while not input_is_valid:
         try:
@@ -40,4 +52,13 @@ def prompt_other_participants()->bool:
         except ValueError as err:
             print(err)
 
-prompt_other_participants()
+def clear():
+      
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+  
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+        
