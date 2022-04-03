8616a233c87e1f53343ac9e5b1627e9be4268761
shadman <thakurshadman@outlook.com>
2022-04-03 02:50:03 -0400

    bust condition and dealer hit condition function added

--- 21/21.py
+++ 21/21.py
@@ -3,12 +3,12 @@ import random
 def build_cards_dict() -> dict :
     
     cards_dict:dict = {}
-    for i in range(1,11):
+    for i in range(2,11):
         cards_dict[str(i)] = i
     cards_dict['J'] = 10
     cards_dict['Q'] = 10
     cards_dict['K'] = 10
-    cards_dict['A'] = (1,11)
+    cards_dict['A'] = 11
     return cards_dict
 
 def init_dealing(card_dict, num_of_cards)->list:
@@ -24,15 +24,11 @@ def get_hand_total(hand,cards_dict)->int:
     
     for individual_card in hand:
         if individual_card == 'A':
+            current_total += cards_dict[individual_card]
             ace_counter += 1
         else:
             current_total += cards_dict[individual_card]
             
-    if ace_counter == 0:
-        return current_total
-    
-    current_total += (11*ace_counter)
-    
     while current_total > 21:
         if ace_counter == 0:
             break
@@ -43,41 +39,61 @@ def get_hand_total(hand,cards_dict)->int:
     return current_total
         
 
-def promptHit()->bool:
+def prompt_hit()->bool:
     acceptable_inputs = {'y':True, 'yes':True,  'hit': True,  'n':False,  'no': False, 'stand':False }
     while True:
         try:
             player_choice = input('Type "y" if you would like to HIT or type "n" if you would like to STAND: ')
+            print('\n')
             return acceptable_inputs[player_choice.lower()]
         except:
             print('Invalid Input\n')
     
 
+def dealers_hits(dealers_score)->bool:
+    if dealers_score < 17:
+        return True
+    else:
+        return False
     
+def check_who_won(player_score, dealers_score):
+        #note for win condition dealer and player could both have 21
+    if player_score == dealers_score:
+        print('DRAW!')
+    elif player_score == 21:
+        print('Black Jack!\n')
+    elif dealers_score > player_score :
+        print('Better luck next time!\n')
+    else:
+        player_choice = prompt_hit()
+        return player_choice
+    
+def check_if_bust(score) -> bool:
+    if score > 21:
+        print('Bust!')
+        return True
     
 def play_game():
     
     deck = build_cards_dict()
     players_hand = init_dealing(deck, 2)
     dealers_hand = init_dealing(deck, 2)
-    player_score = get_hand_total(players_hand,deck)
-    dealers_score = get_hand_total(dealers_hand,deck)
-    print(f'Your Hand: {players_hand} Current Total: {player_score}' )
-    print(f"Dealer's Hand: [{dealers_hand[0]}, ?]")
-    player_choice:bool
-    if player_score == 21:
-        print('Black Jack! You Win!\n')
-    elif dealers_hand == 21:
-        print('Better luck next time!\n')
-    else:
-        player_choice = promptHit()
-    if player_choice:
-        players_hand.extend(init_dealing(deck,1))
-        player_score = get_hand_total(players_hand,deck)
-        print(f'Your Hand: {players_hand} Current Total: {player_score}' )
-        
-        
+    concealed_dealers_hand = [dealers_hand[0], '?']
+    player_choose_hit = True #just for testing
+    dealer_choose_hit = True
     
-
+    while player_choose_hit or dealer_choose_hit:
+        player_score = get_hand_total(players_hand,deck)
+        dealers_score = get_hand_total(dealers_hand,deck)
+        print(f'Your Hand: {players_hand} Current Total: {player_score}' )
+        print(f"Dealer's Hand: {concealed_dealers_hand}, {dealers_score}\n")
+        player_choose_hit = prompt_hit()
+        dealer_choose_hit = dealers_hits(dealers_score)
+        if player_choose_hit:
+            players_hand.extend(init_dealing(deck,1))
+        if dealer_choose_hit:
+            dealers_hand.extend(init_dealing(deck,1))
+            concealed_dealers_hand.append('?')
+    #Evaluate Score After exiting while-lopp
     
 play_game()
